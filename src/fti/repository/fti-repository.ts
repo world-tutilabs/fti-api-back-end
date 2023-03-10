fd25f530964cb3ef3ee180252e9d695ebc3c59c8
yan-levy <yanbatistacel@gmail.com>
2023-03-10 10:18:37 -0400

    feat: add fti repository (findAll)

--- src/fti/repository/fti-repository.ts
+++ src/fti/repository/fti-repository.ts
@@ -1,5 +1,4 @@
 import { getAllFtiDto } from './../dto/get-all-fti.dto';
 export abstract class FtiRepository {
-  abstract findAllEmAprovacao(): Promise<getAllFtiDto[] | null>;
-  abstract findAllHomologadas(): Promise<getAllFtiDto[] | null>;
+  abstract findAll(statusId: number): Promise<getAllFtiDto[] | null>;
 }
