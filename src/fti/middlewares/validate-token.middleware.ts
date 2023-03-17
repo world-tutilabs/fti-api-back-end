f9bead93a0e60ccb926bb99026e697fa7fcebe64
Yan Batista <Yan.Batista@tutiplast.local>
2023-03-17 10:55:59 -0400

    feat: add middleware to validate user's role

---
+++ src/fti/middlewares/validate-token.middleware.ts
@@ -0,0 +1,31 @@
+import { httpUserSystem } from './../../utils/user-system-api';
+import {
+  HttpException,
+  HttpStatus,
+  Injectable,
+  NestMiddleware,
+} from '@nestjs/common';
+
+@Injectable()
+export class ValidateTokenMiddleware implements NestMiddleware {
+  async use(req: any, res: any, next: () => void) {
+    const user = await httpUserSystem.post(
+      '/session/verify',
+      {},
+      { headers: { Authorization: req.headers.authorization } },
+    );
+
+    const role = user.data.user.nivel_de_acesso.descricao;
+
+    if (!req.headers.authorization)
+      throw new HttpException('No Authorization Token', HttpStatus.FORBIDDEN);
+
+    if (role === 'eng_analista' || role === 'eng_admin' || role === 'eng')
+      next();
+    else
+      throw new HttpException(
+        'Invalid Authorization Token',
+        HttpStatus.FORBIDDEN,
+      );
+  }
+}
