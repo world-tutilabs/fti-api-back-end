725bf9d1d5ad67b27755e2c64c68e93fb3c42b77
tutilabs-engineering <88342823+tutilabs-engineering@users.noreply.github.com>
2023-03-24 11:22:39 -0400

    Update fti-repository.ts

--- src/fti/repository/fti-repository.ts
+++ src/fti/repository/fti-repository.ts
@@ -1,7 +1,6 @@
 import { Fti } from '@prisma/client';
 import { HomologDto } from '../types/dto/homolog-fti.dto';
 import { CreateFtiDto } from '../types/dto/create-fti.dto';
-#bugas
 export abstract class FtiRepository {
   abstract listAllByStatus(id: number): Promise<Partial<Fti[]>>;
   abstract create(data: CreateFtiDto, files: any): Promise<Partial<Fti>>;
