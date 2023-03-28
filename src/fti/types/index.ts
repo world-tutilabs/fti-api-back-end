f343ce313f6bca2fcf61d7d24478eb20c90f9434
Yan Batista <Yan.Batista@tutiplast.local>
2023-03-28 11:12:08 -0400

    refactor: improving the importations

---
+++ src/fti/types/index.ts
@@ -0,0 +1,7 @@
+export * from './dto/create-fti.dto';
+export * from './dto/get-all-fti.dto';
+export * from './dto/homolog-fti.dto';
+export * from './dto/update-fti.dto';
+export * from './params/find-by-id';
+export * from './params/find-by-status';
+export * from './params/history';
