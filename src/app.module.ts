a42842eae3663e53bf0536770c5020dedddfe33a
Yan Batista <Yan.Batista@tutiplast.local>
2023-05-02 08:12:54 -0400

    #

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,8 @@ import { ConfigModule } from '@nestjs/config';
 import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
 import { FtiModule } from './fti/fti.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
   imports: [
@@ -10,6 +12,9 @@ import { FtiModule } from './fti/fti.module';
     FtiModule,
     ConfigModule.forRoot({
       isGlobal: true,
+    }),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '../uploads'),
     }),
     FtiModule,
     MailModule,
